d77ec3074860b445af66c820327e8c31cfdc2e2f
r-scott25 <rachelbernal2@gmail.com>
2022-12-22 16:11:37 -0600

    view functions state => new state

--- src/PlayNumber.js
+++ src/PlayNumber.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./PlayNumber.css";
 import "./StarMatch.js";
+import colors from "./colors.js";
+
 
 const PlayNumber = (props) => (
-  <button className="number" onClick={() => console.log("Num", props.number)}>
+  <button className="number" style={{ backgroundColor: colors[props.status]}} onClick={() => console.log("Num", props.number)}>
     {props.number}
   </button>
 );
