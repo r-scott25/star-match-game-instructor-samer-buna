414382de4d4d3a2d198d2fc6e56e5624ec94e6b7
r-scott25 <rachelbernal2@gmail.com>
2022-12-22 16:50:47 -0600

    behavior functions state => new state

--- src/PlayNumber.js
+++ src/PlayNumber.js
@@ -3,9 +3,12 @@ import "./PlayNumber.css";
 import "./StarMatch.js";
 import colors from "./colors.js";
 
-
 const PlayNumber = (props) => (
-  <button className="number" style={{ backgroundColor: colors[props.status]}} onClick={() => console.log("Num", props.number)}>
+  <button
+    className="number"
+    style={{ backgroundColor: colors[props.status] }}
+    onClick={() => props.onClick(props.number, props.status)}
+  >
     {props.number}
   </button>
 );
--- src/StarMatch.js
+++ src/StarMatch.js
@@ -21,6 +21,24 @@ const StarMatch = () => {
     return "available";
   };
 
+  const onNumberClick = (number, currentStatus) => {
+    if (currentStatus == "used") {
+      return;
+    }
+    const newCandidateNums = currentStatus === "available"
+    ? candidateNums.concat(number) : candidateNums.filter(cn => cn !== number);
+    if (utils.sum(newCandidateNums) !== stars) {
+      setCandidateNums(newCandidateNums);
+    } else {
+      const newAvailableNums = availableNums.filter(
+        (n) => !newCandidateNums.includes(n)
+      );
+      setStars(utils.randomSumIn(newAvailableNums, 9));
+      setAvailableNums(newAvailableNums);
+      setCandidateNums([]);
+    }
+  };
+
   return (
     <div className="game">
       <div className="help">
@@ -36,6 +54,7 @@ const StarMatch = () => {
               key={number}
               status={numberStatus(number)}
               number={number}
+              onClick={onNumberClick}
             />
           ))}
         </div>
